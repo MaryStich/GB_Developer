cb9467429d43b8adde955b5132159ba2edb692d2
Mary <mariyastch@gmail.com>
2023-11-21 22:14:30 +0300

    Python lessons added

---
+++ Lec/main.py
@@ -0,0 +1,36 @@
+# n = 1.89
+# print(n)
+
+# n = "hjd\'di"
+# print(n)
+
+# x = 8
+# print(type(x))
+
+# a = 5
+# b = 7.9
+# c = "hello world"
+
+# #print(f"{a} - {b} - {c}")
+
+# print("{} - {} - {}".format(a,b,c))
+
+# print('введите первое число: ')
+# a = int(input())
+# b=int(input("введите второе число: "))
+
+# print(a, '+',b, '=', a+b)
+
+# c=7.63
+# print(c)
+# n=int(c)
+# print(n)
+
+# a = 7.737
+# b = 2.087
+# print(round(a * b, 3))
+
+# a='qwerty'
+# for i in a:
+#     print(i)
+
